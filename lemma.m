f4caa05f0d5b99f6d04e3ad0f7191ffefde6a8a8
smithconor547 <smithconor547@gmail.com>
2020-03-02 10:58:09 -0700

    Pre-processing function skeletons

---
+++ lemma.m
@@ -0,0 +1,7 @@
+function [dictForm] = lemma(words)
+%LEMMA Groups inflected forms of a word so they can be analyzed as a single
+%item
+%   Detailed explanation goes here
+dictForm = words;
+end
+
---
+++ porterStem.m
@@ -0,0 +1,6 @@
+function [stems] = porterStem(words)
+%PORTERSTEM Stems words in an array based on the Porter algorithm
+%   Detailed explanation goes here
+stems = words;
+end
+
