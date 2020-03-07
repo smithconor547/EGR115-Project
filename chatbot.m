c85d382be1a6016a9d8d98df35374ee519136c92
Conor Smith <smithconor547@gmail.com>
2020-03-07 08:20:25 -0700

    Some work on the token function

--- chatbot.m
+++ chatbot.m
@@ -1,3 +1,4 @@
 % Main program file for the chatbot
 % Handles I/O, everything else is handled through function calls
 
+
--- token.m
+++ token.m
@@ -1,8 +1,25 @@
-function [wordArray] = token(words)
+function [out] = token(inString)
 %TOKEN Takes a string and removes all spaces, punctuation, converts to
-%lowercase, returns array of words
+%lowercase, returns a string with processed words delimited by whitespace
 
 % Use ~isstrprop, iterate through the input string removing punctuation,
 % generate an array when whitespace is encountered.
+
+% Lowercase
+
+% Remove punctuation
+strip = '';% Stripped string
+for k = 1:length(inString)
+	if isalpha(inString(k)) || isspace(inString(k))
+		strip = strip + inString(k);
+
+out = inString;
+
+% Slice string into array of words for Porter algorithm processing
+
+% Recombine into whitespace delimeted string
+% NOTE: This output requirement may change based on the implementation
+% of the response generation
+
 end
 
