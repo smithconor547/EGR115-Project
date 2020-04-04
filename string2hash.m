bc46f6eae53164aa6fc63b6dadbc56e1480dbb48
smithconor547 <smithconor547@gmail.com>
2020-04-04 12:32:05 -0700

    Update string2hash.m

--- string2hash.m
+++ string2hash.m
@@ -33,12 +33,12 @@ if(nargin<2), type='djb2'; end
 switch(type)
     case 'djb2'
         hash = 5381*ones(size(str,1),1); 
-        for i=1:size(str,2), 
+        for i=1:size(str,2)
             hash = mod(hash * 33 + str(:,i), 2^32-1); 
         end
     case 'sdbm'
         hash = zeros(size(str,1),1);
-        for i=1:size(str,2), 
+        for i=1:size(str,2)
             hash = mod(hash * 65599 + str(:,i), 2^32-1);
         end
     otherwise
